bcbdc97259ab1db40241654c005c7df4546d0b19
Suryadevelops <75428274+Suryadevelops@users.noreply.github.com>
2021-01-13 16:44:11 +0530

    Initial commit

---
+++ src/resolvers/post.ts
@@ -0,0 +1,22 @@
+import { Post } from '../entities/Post'
+import { Resolver, Query, Ctx, Arg, Int } from "type-graphql"
+import { MyContext } from 'src/types'
+
+
+@Resolver()
+export class PostResolver {
+	@Query(() => [Post])
+	posts(
+		@Ctx() { em }: MyContext
+	): Promise<Post[]> {
+		return em.find(Post, {});
+	}
+
+	@Query(() => Post, { nullable: true })
+	post(
+		@Arg('id', () => Int) id: number,
+		@Ctx() { em }: MyContext
+	): Promise<Post | null> {
+		return em.findOne(Post, { id });
+	}
+}
